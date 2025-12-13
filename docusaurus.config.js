69137de6dd5c4d51ac689db70f6ffb8167ab38e8
Martim Teixeira <a89796@ualg.pt>
2025-12-13 16:20:37 +0000

    Adiciondado link para o github da aplicaç

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,7 +100,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/martimteixeiraa89796/gestao-recessao-hotel',
             label: 'GitHub',
             position: 'right',
           },
